92d273e64b1473c0e9312185f3e76902a9122c42
Neill Russell <git@enru.co.uk>
2021-06-03 10:18:37 +0100

    Update FindRestStorePlugin.php
    Bugfix for 'all' store code

--- Plugin/StoreResolver/FindRestStorePlugin.php
+++ Plugin/StoreResolver/FindRestStorePlugin.php
@@ -18,11 +18,13 @@ class FindRestStorePlugin
         $pathParts = explode('/', trim($pathInfo, '/'));
         if('rest' == current($pathParts)) {
             if($storeCode = next($pathParts)) {
-                try {
-                    $store = $this->storeRepository->getActiveStoreByCode($storeCode);
-                    return $store->getId();
-                } catch (\Magento\Store\Model\StoreIsInactiveException $e) {
-                    //throw new \Magento\Framework\Exception\NoSuchEntityException(__('Requested store is inactive'));
+                if($storeCode != \Magento\Webapi\Controller\PathProcessor::ALL_STORE_CODE) {
+                    try {
+                        $store = $this->storeRepository->getActiveStoreByCode($storeCode);
+                        return $store->getId();
+                    } catch (\Magento\Store\Model\StoreIsInactiveException $e) {
+                        //throw new \Magento\Framework\Exception\NoSuchEntityException(__('Requested store is inactive'));
+                    }
                 }
             }
         }
