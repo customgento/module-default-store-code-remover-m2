2cad7413c32a0df136ff94f1402932bf277fee0d
Iman Aboheydary <iranimij@gmail.com>
2024-01-25 13:06:38 +0100

    Add a new check, DEV-244

---
+++ Helper/StoreUrl.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CustomGento\DefaultStoreCodeRemover\Helper;
+
+use Magento\Store\Api\Data\StoreInterface;
+use Magento\Store\Model\Store;
+use MageWorx\SeoBase\Helper\StoreUrl as MageworxStoreUrl;
+
+class StoreUrl extends MageworxStoreUrl
+{
+    /**
+     * @param StoreInterface $store
+     * @return bool
+     */
+    protected function isUseStoreCodeInUrl(StoreInterface $store): bool
+    {
+        if ($store->getCode() !== Store::ADMIN_CODE && $store->isDefault()) {
+            return false;
+        }
+
+        $storeId = (int)$store->getId();
+
+        return !($store->hasDisableStoreInUrl() && $store->getDisableStoreInUrl())
+            && $this->configDataLoader->getConfigValue(Store::XML_PATH_STORE_IN_URL, $storeId);
+    }
+}
