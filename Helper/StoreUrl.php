4db4223dd0350d2fef9ddc560c96d98e5ca30700
Rana Muhammad Irshad <rana@customgento.com>
2024-02-01 11:33:30 +0100

    Improve code to handle errors, DEV-362

--- Helper/StoreUrl.php
+++ Helper/StoreUrl.php
@@ -6,19 +6,19 @@ namespace CustomGento\DefaultStoreCodeRemover\Helper;
 
 use Magento\Store\Api\Data\StoreInterface;
 use Magento\Store\Model\Store;
-use MageWorx\SeoBase\Helper\StoreUrl as MageworxStoreUrl; /** @phpstan-ignore-line */
+use MageWorx\SeoBase\Helper\StoreUrl as MageworxStoreUrl;
 
-/** @phpstan-ignore-next-line */
-class StoreUrl extends MageworxStoreUrl /** @phpstan-ignore-line */
+class StoreUrl extends MageworxStoreUrl
 {
     protected function isUseStoreCodeInUrl(StoreInterface $store): bool
     {
+        /** @phpstan-ignore-next-line */
         if ($store->getCode() !== Store::ADMIN_CODE && $store->isDefault()) {
             return false;
         }
 
         $storeId = (int)$store->getId();
-
+        /** @phpstan-ignore-next-line */
         return !($store->hasDisableStoreInUrl() && $store->getDisableStoreInUrl())
             && $this->configDataLoader->getConfigValue(Store::XML_PATH_STORE_IN_URL, $storeId);
     }
