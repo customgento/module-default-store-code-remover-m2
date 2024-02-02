ce8095380aefd5acd10eedbe4ba12cd9590f394a
Rana Muhammad Irshad <rana@customgento.com>
2024-02-02 10:11:23 +0100

    Handle errors in mageworx, DEV-362

--- Helper/StoreUrl.php
+++ Helper/StoreUrl.php
@@ -8,17 +8,30 @@ use Magento\Store\Api\Data\StoreInterface;
 use Magento\Store\Model\Store;
 use MageWorx\SeoBase\Helper\StoreUrl as MageworxStoreUrl;
 
+if (!class_exists(MageworxStoreUrl::class)) {
+    return;
+}
+
 class StoreUrl extends MageworxStoreUrl
 {
     protected function isUseStoreCodeInUrl(StoreInterface $store): bool
     {
-        /** @phpstan-ignore-next-line */
+        if (!method_exists($store, 'isDefault')) {
+            return false;
+        }
+
         if ($store->getCode() !== Store::ADMIN_CODE && $store->isDefault()) {
             return false;
         }
 
         $storeId = (int)$store->getId();
-        /** @phpstan-ignore-next-line */
+        if (!method_exists($store, 'hasDisableStoreInUrl') || !method_exists($store, 'getDisableStoreInUrl')) {
+            return false;
+        }
+
+        if (empty($this->configDataLoader)) {
+            return false;
+        }
         return !($store->hasDisableStoreInUrl() && $store->getDisableStoreInUrl())
             && $this->configDataLoader->getConfigValue(Store::XML_PATH_STORE_IN_URL, $storeId);
     }
