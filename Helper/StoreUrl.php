7ed5ef756d9d6699925a19ba82d5489b5178405f
Rana Muhammad Irshad <rana@customgento.com>
2024-02-10 22:21:43 +0100

    Improve class exists, DEV-362

--- Helper/StoreUrl.php
+++ Helper/StoreUrl.php
@@ -8,48 +8,19 @@ use Magento\Store\Api\Data\StoreInterface;
 use Magento\Store\Model\Store;
 use MageWorx\SeoBase\Helper\StoreUrl as MageworxStoreUrl;
 
-// Check if MageWorx\SeoBase\Helper\StoreUrl class exists
-$mageWorxClassExists = class_exists(MageworxStoreUrl::class);
-
-// Define the StoreUrl class based on whether MageWorx class exists
-if ($mageWorxClassExists) {
+if (class_exists(MageWorxStoreUrl::class)) {
     class StoreUrl extends MageworxStoreUrl
     {
         protected function isUseStoreCodeInUrl(StoreInterface $store): bool
         {
-            if (!method_exists($store, 'isDefault')) {
-                return false;
-            }
-
             if ($store->getCode() !== Store::ADMIN_CODE && $store->isDefault()) {
                 return false;
             }
 
             $storeId = (int)$store->getId();
-            if (!method_exists($store, 'hasDisableStoreInUrl') || !method_exists($store, 'getDisableStoreInUrl')) {
-                return false;
-            }
-
-            if (empty($this->configDataLoader)) {
-                return false;
-            }
 
             return !($store->hasDisableStoreInUrl() && $store->getDisableStoreInUrl())
-                && $this->configDataLoader->getConfigValue(
-                    Store::XML_PATH_STORE_IN_URL,
-                    $storeId
-                );
-        }
-    }
-} else {
-    // Handle the case when MageWorx\SeoBase\Helper\StoreUrl class is not found
-    class StoreUrl
-    {
-        // Provide an alternative implementation or leave it empty based on your needs
-        protected function isUseStoreCodeInUrl(StoreInterface $store): bool
-        {
-            // Your alternative logic here
-            return false;
+                && $this->configDataLoader->getConfigValue(Store::XML_PATH_STORE_IN_URL, $storeId);
         }
     }
 }
