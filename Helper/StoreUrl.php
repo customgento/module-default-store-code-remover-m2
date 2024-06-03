7fd5026f0277fbee65e8761976665e5954574fa1
norgeindian <ole@customgento.com>
2024-06-03 13:20:45 +0200

    Fix mageworx dependency, DEV-362

--- Helper/StoreUrl.php
+++ Helper/StoreUrl.php
@@ -8,17 +8,25 @@ use Magento\Store\Api\Data\StoreInterface;
 use Magento\Store\Model\Store;
 use MageWorx\SeoBase\Helper\StoreUrl as MageworxStoreUrl;
 
-class StoreUrl extends MageworxStoreUrl
-{
-    protected function isUseStoreCodeInUrl(StoreInterface $store): bool
+if (class_exists(MageworxStoreUrl::class)) {
+    class StoreUrl extends MageworxStoreUrl
     {
-        if ($store->getCode() !== Store::ADMIN_CODE && $store->isDefault()) {
-            return false;
+        protected function isUseStoreCodeInUrl(StoreInterface $store): bool
+        {
+            if ($store->getCode() !== Store::ADMIN_CODE && $store->isDefault()) {
+                return false;
+            }
+
+            $storeId = (int)$store->getId();
+
+            return !($store->hasDisableStoreInUrl() && $store->getDisableStoreInUrl())
+                // @phpstan-ignore-next-line
+                && $this->configDataLoader->getConfigValue(Store::XML_PATH_STORE_IN_URL, $storeId);
         }
+    }
+} else {
+    class StoreUrl
+    {
 
-        $storeId = (int)$store->getId();
-
-        return !($store->hasDisableStoreInUrl() && $store->getDisableStoreInUrl())
-            && $this->configDataLoader->getConfigValue(Store::XML_PATH_STORE_IN_URL, $storeId);
     }
 }
